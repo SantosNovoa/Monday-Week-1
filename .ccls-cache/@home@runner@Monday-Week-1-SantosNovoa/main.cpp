e09f1e01a4135457d2d2531a67a9d0fbd2e38935
Santos Novoa <124646185+SantosNovoa@users.noreply.github.com>
2023-02-07 04:53:33 +0000

    Added the string library

--- .ccls-cache/@home@runner@Monday-Week-1-SantosNovoa/main.cpp
+++ .ccls-cache/@home@runner@Monday-Week-1-SantosNovoa/main.cpp
@@ -1,11 +1,12 @@
 // Monday-Week-1
 // Due: Friday, February 10, 11:59 PM
 /*************************************************
-Student First Name: 
-Student Last Name: 
+Student First Name: Santos
+Student Last Name: Novoa
 **************************************************/
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
